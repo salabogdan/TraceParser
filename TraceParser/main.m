b01b6681295bb0554a7ec7c8f9c7306e9de2cb2f
Bogdan Sala <bogdansala@Bogdans-MacBook-Pro.local>
2015-03-18 11:12:22 +0200

    Initial Commit

---
+++ TraceParser/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TraceParser
+//
+//  Created by Bogdan Sala on 18/03/15.
+//  Copyright (c) 2015 Bogdan Sala. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
